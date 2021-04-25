28db8d5037fff36c80c6de6418a6c2fd5de47f4d
JulianeVetter <juliane.vetter@studmail.htw-aalen.de>
2021-04-25 16:35:48 +0200

    added attributes of camera, also in calibration.py

--- camera_calibration/camera.py
+++ camera_calibration/camera.py
@@ -2,11 +2,14 @@ import cv2 #so far unused in this file
 
 # definition of the class Camera
 # 
-def class Camera(self)
+class Camera(object):
 #todo: entscheiden ob private/public/property etc. 
 
-#initialisation
-#attributes
+    def __init__(self): 
+        self.matrix = np.zeros(3,3)     #camera matrix
+        self.dist = np.zeros(5,1)       #distortion coefficients
+
+    #evtl. setter definieren, manche Felder von matrix sind ja immer 0 
 
 #methods
 pass 
