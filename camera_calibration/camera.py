27a8742bfc64831a79af12f789372fafb60be87b
JulianeVetter <juliane.vetter@studmail.htw-aalen.de>
2021-05-09 16:00:53 +0200

    calibrate as staticmethod of class camera

--- camera_calibration/camera.py
+++ camera_calibration/camera.py
@@ -1,5 +1,7 @@
-import cv2 #so far unused in this file
-import numpy as np  
+import cv2 
+import numpy as np 
+import os
+import glob  
 
 # definition of the class Camera
 # 
@@ -34,7 +36,100 @@ class Camera(object):
     matrix = property(get_matrix, set_matrix)
     dist = property(get_dist, set_dist)
 
-    #evtl. setter definieren, manche Felder von matrix sind ja immer 0 
+    @staticmethod
+    def calibrate():
+
+        # Define the dimensions of checkerboard
+        CHECKERBOARD = (6, 9)
+
+        # stop the iteration when specified accuracy, epsilon, is reached or specified number of iterations are completed.
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+        # Vector for 3D points
+        threedpoints = []
+
+        # Vector for 2D points
+        twodpoints = []
+
+        # 3D points real world coordinates
+        objectp3d = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+        objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+        prev_img_shape = None
+
+        # Extracting path of individual image stored in a given directory. 
+        # Since no path is specified, it will take current directory jpg files alone
+        images = glob.glob('*.jpg', recursive=True)
+        #print(images)
+
+        for filename in images:
+            global image
+            image = cv2.imread(filename)
+            grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # Find the chess board corners
+            # If desired number of corners are found in the image then ret = true
+            ret, corners = cv2.findChessboardCorners( grayColor, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH
+                            + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+
+            # If desired number of corners can be detected then, 
+            # refine the pixel coordinates and display them on the images of checker board
+            # if ret == True:
+            #     threedpoints.append(objectp3d)
+
+            #     # Refining pixel coordinates
+            #     # for given 2d points.
+            #     corners2 = cv2.cornerSubPix(
+            #         grayColor, corners, (11, 11), (-1, -1), criteria)
+
+            #     twodpoints.append(corners2)
+
+            #     # Draw and display the corners
+            #     image = cv2.drawChessboardCorners(image,
+            #                                     CHECKERBOARD,
+            #                                     corners2, ret)
+
+            cv2.imshow('img', image)
+            cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+
+        h, w = image.shape[:2]
+
+
+        # Perform camera calibration by
+        # passing the value of above found out 3D points (threedpoints)
+        # and its corresponding pixel coordinates of the
+        # detected corners (twodpoints)
+        #ret = return value 
+        ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(
+            threedpoints, twodpoints, grayColor.shape[::-1], None, None)
+
+        # Save attributes in an object
+        # todo: anpassen, da es jetzt eine Methode ist!! 
+        CurrentCamera = ca.Camera()
+        CurrentCamera.set_matrix(matrix)
+        CurrentCamera.set_dist(distortion)
+
+        # Displaying required output
+        print(" Camera matrix:")
+        print(matrix)
+
+        print("\n Distortion coefficient:")
+        print(distortion)
+
+        print("\n Rotation Vectors:")
+        print(r_vecs)
+
+        print("\n Translation Vectors:")
+        print(t_vecs)
+
+        #for developping purpose
+        print("\n Camera matrix in CurrentCamera")
+        print(CurrentCamera.get_matrix())
+
+        print("\n Distortion coefficients in CurrentCamera")
+        print(CurrentCamera.get_dist())
+
 
 #methods
 pass 
