5ae5c1176c3efbdbf77bd01046a3e5803459104c
Markus von Staden <markus@von-staden.net>
2021-05-22 13:41:34 +0200

    Clean Up

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import numpy as np
 import cv2
 from image_capturing.image_capturing import get_blackboard_or_none
 import camera_calibration.camera as ca 
-#import camera_calibration.calibration as cc
-#import camera_calibration.camera
 
 camera_changed = True #for developping purpose; will later be set by GUI
 
 if camera_changed == True: 
-    #NewCamera = ca.Camera()
-    #NewCamera.calibrate()
     CurrentCamera = ca.Camera.calibrate()
     print("CurrentCamera.dist: ")
     print(CurrentCamera.dist)
@@ -19,12 +15,10 @@ if camera_changed == True:
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
 
     blackboard = get_blackboard_or_none(frame)
 
-    # Display the resulting frame
     cv2.imshow('frame',blackboard)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
