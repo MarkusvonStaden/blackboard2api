a6aa5e8319cd22e7c3dc5cd0665bb65d5965ec08
Markus von Staden <markus@von-staden.net>
2021-06-19 13:10:03 +0200

    update path in test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
-from ..main import Main
+import sys
+sys.path.append("./")
+import main
 def test_video_returns_three_boards():
-    instance = Main(recalibrate_camera = False, path = "test.mp4")
+    instance = main.Main(recalibrate_camera = False, path = "test.mp4")
     result = instance.loop()
     assert len(result) == 3
