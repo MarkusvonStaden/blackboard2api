7ac4251bc0b9b54ce04cdf7869b8467c667c3969
Markus von Staden <markus@von-staden.net>
2021-06-19 13:16:10 +0200

    use other test video

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,6 @@ import sys
 sys.path.append("./")
 import main
 def test_video_returns_three_boards():
-    instance = main.Main(recalibrate_camera = False, path = "test.mp4")
+    instance = main.Main(recalibrate_camera = False, path = "Compressed_Test.mp4")
     result = instance.loop()
     assert len(result) == 3
